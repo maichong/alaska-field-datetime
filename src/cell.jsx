027108315e04c835b68da66a275ecf76d64b11ca
Liang Xingchen <liang@maichong.it>
2016-04-20 17:00:29 +0800

    fix bug : cell empty value

--- src/cell.jsx
+++ src/cell.jsx
@@ -19,6 +19,9 @@ export default class DatetimeFieldCell extends React.Component {
 
   render() {
     let props = this.props;
+    if (!props.value) {
+      return <div></div>;
+    }
     return (
       <div>
         {moment(props.value).format(props.field.format)}
